b7dd797c672fcc7c86d4a8c39b0a3f65c6d5e578
CodeFlow <realrajapaksha@gmail.com>
2020-12-29 23:48:18 +0530

    1st
    * Clean build and setup environment

---
+++ Payment&Notify/paymentandnotify/src/main/java/com/nibm/EADCW/paymentandnotify/PaymentandnotifyApplication.java
@@ -0,0 +1,13 @@
+package com.nibm.EADCW.paymentandnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentandnotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentandnotifyApplication.class, args);
+	}
+
+}
---
+++ Payment&Notify/paymentandnotify/src/test/java/com/nibm/EADCW/paymentandnotify/PaymentandnotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nibm.EADCW.paymentandnotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentandnotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
