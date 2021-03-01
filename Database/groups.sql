9b4660d877e7c322bf855939d0cdf28affbe0267
CodeFlow <realrajapaksha@gmail.com>
2021-03-01 23:54:02 +0530

    database attached

--- Database/groups.sql
+++ Database/groups.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 20, 2021 at 03:31 PM
+-- Generation Time: Mar 01, 2021 at 06:23 PM
 -- Server version: 8.0.21
 -- PHP Version: 7.3.21
 
@@ -24,28 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `payment`
+-- Table structure for table `groups`
 --
 
-DROP TABLE IF EXISTS `payment`;
-CREATE TABLE IF NOT EXISTS `payment` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(20) NOT NULL,
-  `date` date NOT NULL,
-  `time` int NOT NULL,
-  `price` int NOT NULL,
-  `seller` varchar(15) NOT NULL,
-  `method` varchar(10) NOT NULL,
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `startDate` varchar(20) NOT NULL,
+  `startTime` varchar(20) NOT NULL,
+  `endDate` varchar(20) NOT NULL,
+  `endTime` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=50002 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `payment`
+-- Dumping data for table `groups`
 --
 
-INSERT INTO `payment` (`id`, `email`, `date`, `time`, `price`, `seller`, `method`) VALUES
-(50000, 'test@email.com', '2021-01-01', 1230, 10000, 'kamal', 'card'),
-(50001, 'test@email.com', '2021-01-01', 1230, 10000, 'kamal', 'card');
+INSERT INTO `groups` (`id`, `name`, `username`, `password`, `startDate`, `startTime`, `endDate`, `endTime`) VALUES
+(1111, 'names', 'realrajapaksha', '1234', '2021/2/27', '08:00 PM', '2021/2/27', '08:00 PM');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
